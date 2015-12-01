cbf767a52b5cfb8db4c7a26bf7fcf3ee45e2f347
harley-travis <harley.travis2@gmail.com>
2015-12-01 15:06:48 -0700

    Update budget.php
    Here are the changes thus far. There are still a bunch of issues that need to be corrected. We can go over them tonight. I will continue to work on it before our call. 
    
    Right now this code:
    
    1. appends new input fields for data.
    2. saves/stores/loads 1 field of information

--- pages/budget.php
+++ pages/budget.php
@@ -11,40 +11,69 @@ Budget Page of Kiss My Budget App
         <meta name="author" content="Krystal Snyder">
         <meta name="description" content="This is a mobile application being developed by BYU-I students to use for budgetting.">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <script>
+            function budgetList(){
+                
+                // get span tag. span tags only include the input tag
+                var elmnt = document.getElementsByTagName("SPAN")[0];
+                var cln = elmnt.cloneNode(true);
+                var budgetListing = document.getElementById("budget-listing");
+                budgetListing.appendChild(cln);
+                
+            
+            }
+            // save the data
+            function save(){
+                    var eName = document.getElementById('expenseName').value;
+                    localStorage.setItem('text', eName);
+            }
+                //load the data 
+            function load(){
+                var storedValue = localStorage.getItem('text');
+                if(storedValue){
+                    document.getElementById('expenseName').value = storedValue;
+                }   
+            }
+        </script>
     </head>
-    <body id="home">
-        <div>
-            <header role="banner">
-                <div>
-            
-                </div>
-            </header>
-            <nav role="navigation">
-                <div>
-                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/modularization/nav.php'; ?>
-                </div>
-            </nav>
-            <main role="main">
-                
-                <h1>Your Budget</h1>
-                
-                <p>This is your Budget. You can make goals for coming days, months, and years as well as add and remove budget items.</p>
-            
-               
-            </main>
-            <aside role="complementary">
-                <div>
-                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/modularization/user.php'; ?>
-                </div>
-            </aside>
-            <footer role="contentinfo">
-          
-                <div>
-                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/modularization/footer.php'; ?>
-                </div>
-                
-            </footer>
-        </div>
+    <body id="home" onload="load()">
+        <header role="banner">
+            <div>
+                <h1>Budget</h1>
+            </div>
+        </header>
+        <nav role="navigation">
+            <div>
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modularization/nav.php'; ?>
+            </div>
+        </nav>
+        <main role="main">
+            <h1>Your Budget</h1>
+            <p>This is your Budget. You can make goals for coming days, months, and years as well as add and remove budget items.</p>
+        </main>
+        <aside role="complementary">
+            <div>
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modularization/user.php'; ?>
+            </div>
+        </aside>
+        <div id="budget-list">
+            <button onclick="budgetList()">Add New Row</button>
+            <input type="button" value="save" onclick="save()" />
+            <ul class="title">
+                <li class="title">Item</li>
+                <li class="title">Expected</li>
+                <li class="title">Actual</li>
+            </ul>
+            <ul id="budget-listing">
+                <li><span><input type="text" id="expenseName" ><input type="text" id="expectedExpense" value="enter expected expense"><input type="text" id="actualExpense" value="enter actual expense"></span></li>
+                <li><span><input type="text" id="expenseName" value=""><input type="text" id="expectedExpense" value="enter expected expense"><input type="text" id="actualExpense" value="enter actual expense"></span></li>
+                <li><span><input type="text" id="expenseName" value="enter expensive name"><input type="text" id="expectedExpense" value="enter expected expense"><input type="text" id="actualExpense" value="enter actual expense"></span></li>                    
+            </ul>
+        </div><!-- budget-list -->
+        <footer role="contentinfo">
+            <div>
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modularization/footer.php'; ?>
+            </div>
+        </footer>
     </body>
 </html>
