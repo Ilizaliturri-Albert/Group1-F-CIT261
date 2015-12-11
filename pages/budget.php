7cab1e4ba9bf74cc8966dc3990ec34f401727680
harley-travis <harley.travis2@gmail.com>
2015-12-11 00:29:49 -0700

    Update budget.php
    Updated current code. This code allows you to create a new row, input information, store and load that data back into the screen. There is stil 1 or 2 things that are wrong with it that i need to correct.

--- pages/budget.php
+++ pages/budget.php
@@ -11,30 +11,18 @@ Budget Page of Kiss My Budget App
         <meta name="author" content="Krystal Snyder">
         <meta name="description" content="This is a mobile application being developed by BYU-I students to use for budgetting.">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script>
-            function budgetList(){
-                
-                // get span tag. span tags only include the input tag
-                var elmnt = document.getElementsByTagName("SPAN")[0];
-                var cln = elmnt.cloneNode(true);
-                var budgetListing = document.getElementById("budget-listing");
-                budgetListing.appendChild(cln);
-                
-            
+<style>
+            .rowClass{
+                margin:5px;
             }
-            // save the data
-            function save(){
-                    var eName = document.getElementById('expenseName').value;
-                    localStorage.setItem('text', eName);
+            li{
+                list-style: none;
+                display: inline-block;
+                width: 100%;
+                max-width: 173px;
+                font-weight: bold;
             }
-                //load the data 
-            function load(){
-                var storedValue = localStorage.getItem('text');
-                if(storedValue){
-                    document.getElementById('expenseName').value = storedValue;
-                }   
-            }
-        </script>
+        </style>
     </head>
     <body id="home" onload="load()">
         
@@ -47,12 +35,8 @@ Budget Page of Kiss My Budget App
                 
             </nav>
         <main role="main">
-            <div id="pageheader">
-                <h1>Your Budget<br><br>
-            
-                <div id="orange">BEWARE OF LITTLE EXPENSES;<br> A SMALL LEAK WILL SINK A GREAT SHIP.</div></h1>
-            </div>
-            <p>This is where you can enter all your budget information. Enter the appropriate expenses and their expected and actual cost. You can see where you are spending as expected and where you can or need to make changes to meet your goals.</p>
+            <h1>Your Budget</h1>
+            <p>This is your Budget. You can make goals for coming days, months, and years as well as add and remove budget items.</p>
         </main>
         <aside role="complementary">
             <div>
@@ -60,19 +44,14 @@ Budget Page of Kiss My Budget App
             </div>
         </aside>
         <div id="budget-list">
-            
-<!--            <ul class="title">
-                <li class="title">Item</li>
-                <li class="title">Expected</li>
-                <li class="title">Actual</li>
-            </ul>-->
-            <ul id="budget-listing">
-                <li><span><input type="text" id="expenseName" placeholder="enter expense name" ><input type="text" id="expectedExpense" placeholder="enter expected cost"><input type="text" id="actualExpense" placeholder="enter actual cost"></span></li>
-                <li><span><input type="text" id="expenseName" placeholder="enter expense name" ><input type="text" id="expectedExpense" placeholder="enter expected cost"><input type="text" id="actualExpense" placeholder="enter actual cost"></span></li>
-                <li><span><input type="text" id="expenseName" placeholder="enter expense name" ><input type="text" id="expectedExpense" placeholder="enter expected cost"><input type="text" id="actualExpense" placeholder="enter actual cost"></span></li>
+            <button onclick="addRow()">Add New Row</button>
+            <input type="button" value="Save" onclick="save()" />
+            <ul>
+                <li>Expense Name</li>
+                <li>Expected Amount</li>
+                <li>Actual Amount</li>
             </ul>
-                <button onclick="budgetList()">Add New Row</button>
-            <input type="button" value="save" onclick="save()" />
+            <div id="rowContent"></div>
         </div><!-- budget-list -->
         <footer role="contentinfo">
             <div>
@@ -81,3 +60,85 @@ Budget Page of Kiss My Budget App
         </footer>
     </body>
 </html>
+<script>
+    
+    
+         /* BUDGET APP
+         
+******** 1. add a new set of rows
+******** 2. enter data into input
+******** 3. save data to local storage
+******** 4. load data from local storage inside respective input id's
+******** 5. calculate the total costs of expected/actual columns
+        */
+
+    //a new line of rows
+    function addRow(){
+        
+        var rowNumber = getCurrentCount();
+        var html = "<div class='rowClass'><input id='name-"+rowNumber+"' type='text'><input id='expected-"+rowNumber+"' type='text'><input id='actual-"+rowNumber+"' type='text'></div>";
+        
+        var currentHtml = document.getElementById('rowContent').innerHTML;        
+        document.getElementById('rowContent').innerHTML = currentHtml + html;
+
+    }
+    
+    
+    // count the number of divs under rowContent
+    function getCurrentCount(){
+        var parent = document.getElementById('rowContent');        
+        return parent.getElementsByTagName('div').length;
+    }
+    
+    
+    // save the data
+    function save(rowNumber){
+        
+        var rowNumber = getCurrentCount();
+                
+        // get the dynamic id for #name
+        for(var i = 0; i < rowNumber; i++){
+            var keyName = i;            
+            var inputName = document.getElementById("name-" + [i]).value;
+            var inputExpected = document.getElementById("expected-" + [i]).value;
+            var inputActual = document.getElementById("actual-" + [i]).value;
+            
+            var allIds = [inputName, inputExpected, inputActual];
+            var key_json = JSON.stringify(allIds);
+            localStorage.setItem("Item-"+i, key_json);
+        }
+        
+    }
+    
+    //load the data 
+    function load(){
+        
+        // add a new row for the data that is loaded
+        for(i = 0; i < localStorage.length; i++){
+            addRow();
+            
+        }
+        // loop through the input fields and parse the data
+        for(i = 0; i < localStorage.length; i++){
+
+            
+            var row = localStorage.getItem(localStorage.key(i));
+            var parsed = JSON.parse(row);
+            
+            document.getElementById("name-" + i).value = parsed[0];
+            document.getElementById("expected-" + i).value = parsed[1];
+            document.getElementById("actual-" + i).value = parsed[2];
+            
+        }
+    }
+
+    
+    // initialize the app with a set of rows
+    document.addEventListener("DOMContentLoaded", function(event) { 
+      addRow();
+    });
+</script>
+
+
+Status API Training Shop Blog About Pricing
+© 2015 GitHub, Inc. Terms Privacy Security Contact Help
