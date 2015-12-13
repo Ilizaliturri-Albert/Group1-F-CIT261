3e21ede052ccef57a71ff7082714cb813474f60f
harley-travis <harley.travis2@gmail.com>
2015-12-12 18:47:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -30,8 +30,10 @@ Home Page of Kiss My Budget App
                 <div id="pageheader">
                 <h1>MONEY IS ONLY A TOOL.<br> <span id="purple">IT WILL TAKE YOU WHEREVER YOU WISH,</span><br> <span id="orange">BUT IT WILL NOT REPLACE YOU AS THE DRIVER.</span></h1>
                 </div>
+                <div class="container">
+                    <p>Welcome to Kiss My Budget. An online bugeting app to help you keep tabs on all your expenses. This app will help you manage your debt, savings, and monthly expensives. All you need to do is go to our Budget page and you can begin enterin in the names of the bills and the expected and actual amounts. </p>
+                </div>
                 
-                <p>This is the main page of the budgetting app. More content to come soon!</p>
             
                
             </main>
